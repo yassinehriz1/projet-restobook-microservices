3d183aa781e3afa4613ec528a6c583d22598180e
yassinehriz1 <yassinehriz532@gmail.com>
2025-11-19 01:33:59 +0100

    route 5

--- catalogue-microservice/microservice-backend/server.js
+++ catalogue-microservice/microservice-backend/server.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use('/api/products', productRoutes);
-app.get('/api/current-user', (req, res) => {
+app.get('/api/currentUser', (req, res) => {
   res.json({ message: "hello" });
 });
 
